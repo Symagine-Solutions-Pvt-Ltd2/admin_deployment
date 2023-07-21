b557d2d1a3a1c942bd63d154c244a5ece17a60c2
rimi-here <drimi0979@gmail.com>
2023-07-21 18:13:28 +0530

    TODAY

--- src/Components/FormRow.js
+++ src/Components/FormRow.js
@@ -4,15 +4,17 @@ import Sidebar from "../Sidebar.js"  ;
   
 
 // frame 24 
-function   FormRow() {
+function   FormRow(   props ) {  
+
+  console.log( props.level )  ; 
   return (
     <div className="FormRow">  
   
-     <div className="Description">
-           <p>hjhjgjh </p>
+     <div className="Form-Description">
+           <p>{ props.level}</p>
      </div> 
-     <div className="Input">
-        <input  className="input-box" placeholder="ghjthj" /> 
+     <div className="Form-Input">
+        <input  className="input-box"   /> 
      </div>
     </div>
   );
